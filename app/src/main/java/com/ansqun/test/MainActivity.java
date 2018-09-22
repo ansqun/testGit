827760a35320fa124fbc5df7d157613200055b4b
ansqun <ansqun@qq.com>
2018-09-22 23:43:07 +0800

    gradle test first time
    Change-Id: I0be8bdc570602d335862c0cd8aed748207dd82c6

--- app/src/main/java/com/ansqun/test/MainActivity.java
+++ app/src/main/java/com/ansqun/test/MainActivity.java
@@ -2,12 +2,18 @@ package com.ansqun.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    boolean flag = BuildConfig.testFlag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: flag = [" + flag + "]");
         setContentView(R.layout.activity_main);
+        String buildTime = getString(R.string.build_time);
+        Log.d(TAG, "onCreate: buildTime = [" + buildTime + "]");
     }
 }
